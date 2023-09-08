afcc9e724775489daade0a6d0a20d7ac5c8f4ec1
Abdur Rehman <abdurrehmandev6@gmail.com>
2023-09-08 19:33:38 +0500

    Finalized Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {ReactComponent as Dice} from "./assests/images/icon-dice.svg"
+import { useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState("Click the dice to start generating advice and please wait for 2 seconds before generating again")
+  const [id, setId] = useState("")
+
+  function handleClick(){
+    fetch("	https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => handleRender(data))
+  }
+
+  function handleRender(data){
+    setQuote(data.slip.advice)
+    setId(data.slip.id)
+  }
+
+
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="App">
+        <h5 className="heading">ADVICE #{id}</h5>
+        <p className="advice">
+          "{quote}"
+        </p>
+        <hr className="divider" color="CEE3E9" />
+      </div>
+      <div className="dice-container">
+        <div className="dice" onClick={handleClick}>
+          <Dice />
+        </div>
+      </div>
     </div>
   );
 }

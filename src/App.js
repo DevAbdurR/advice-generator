71f3d3305fe88345cf704b4589de072126027e38
Abdur Rehman <abdurrehmandev6@gmail.com>
2023-09-08 19:55:55 +0500

    Added Error Handling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { useState } from 'react';
 function App() {
   const [quote, setQuote] = useState("Click the dice to start generating advice and please wait for 2 seconds before generating again")
   const [id, setId] = useState("")
+  
 
   function handleClick(){
     fetch("	https://api.adviceslip.com/advice")
       .then((response) => response.json())
       .then((data) => handleRender(data))
+      .catch(() => handleError())
   }
 
   function handleRender(data){
-    setQuote(data.slip.advice)
+    setQuote(`"${data.slip.advice}"`)
     setId(data.slip.id)
+  }
+
+  function handleError(){
+    setQuote(<span className='error-msg'>"There was a problem. Please try again Later."</span>);
+    setId("")
   }
 
 
@@ -23,7 +30,7 @@ function App() {
       <div className="App">
         <h5 className="heading">ADVICE #{id}</h5>
         <p className="advice">
-          "{quote}"
+          {quote}
         </p>
         <hr className="divider" color="CEE3E9" />
       </div>
